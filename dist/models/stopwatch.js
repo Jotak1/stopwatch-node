d6f85c49bced9049a1aec098c7e991585d7828dd
JP <juan.ausensi@haibu.cl>
2022-02-27 23:03:19 -0300

    fix models

--- dist/models/stopwatch.js
+++ dist/models/stopwatch.js
@@ -1,8 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize("postgres://postgres:1234@localhost:5432/stopwatch");
-const User = sequelize.define("users", {
+const config_1 = __importDefault(require("../database/config"));
+const User = config_1.default.define("users", {
     id: {
         type: sequelize_1.DataTypes.INTEGER,
         primaryKey: true,
